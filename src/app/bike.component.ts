fe85906f06904a4f786495c1849dc8cda0a3e4de
ixap2i <ayamashita124+124@gmail.com>
2020-06-22 10:45:00 +0900

    output service logic

--- src/app/bike.component.ts
+++ src/app/bike.component.ts
@@ -1,30 +1,55 @@
-import { ViewChild, OnInit } from '@angular/core';
-
+import { ViewChild, OnInit, Component } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
+import { Observable} from 'rxjs';
+@Component({
+  selector: 'app-bike',
+  styleUrls: ['./bike.component.scss'],
+  template: `
+  <table>
+    <tr *ngFor="let col of displayedColumns">
+      <th>{{col}}</th>
+    </tr>
+    <tr *ngFor="let data of datas();">
+      <td>{{data}}</td>
+    </tr>
+  </table>
+  `
+})
 
-export interface PeriodicElemnt {
+export class BikeComponent implements OnInit {
+
+  displayedColumns: string[] = ['name', 'maker', 'weight', 'class']
+  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+
+  ngOnInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
+  datas() {
+    let keys = Object.keys(this.dataSource.data[0])
+    let arr = [];
+    for (let d of this.dataSource.data) {
+      arr.push(d[keys[0]]);
+      arr.push(d[keys[1]]);
+      arr.push(d[keys[2]]);
+      arr.push(d[keys[3]]);
+    }
+    return arr;
+  }
+}
+export interface PeriodicElement {
   name: string;
   maker: string;
   weight: number;
   class: number;
 }
 
-const ELEMENT_DATA: PeriodicElemnt[] = [
+const ELEMENT_DATA: PeriodicElement[] = [
   { name: 'GSX250R', maker: 'SUZUKI', weight: 134, class: 250},
   { name: 'gixxer', maker: 'SUZUKI', weight: 134, class: 150 },
   { name: 'gixxer sf 250', maker: 'SUZUKI', weight: 184, class: 250 },
   { name: 'R25', maker: 'YAMAHA', weight: 184, class: 250 },
-]
-
-export class BikeComponent implements OnInit {
-  displayedColumns: string[] = ['name', 'maker', 'weight', 'class']
-  dataSource = ELEMENT_DATA;
-
-  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
-
-  ngOnInit() {
-
-    // this.dataSource.paginator = this.paginator;
-  }
-}
+];
--- src/app/bike.module.ts
+++ src/app/bike.module.ts
@@ -1,35 +1,15 @@
 import { OnInit, ViewChild, NgModule } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
+import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
-
-export interface PeriodicElemnt {
-  name: string;
-  maker: string;
-  weight: number;
-  class: number;
-}
-
-const ELEMENT_DATA: PeriodicElemnt[] = [
-  { name: 'GSX250R', maker: 'SUZUKI', weight: 134, class: 250},
-  { name: 'gixxer', maker: 'SUZUKI', weight: 134, class: 150 },
-  { name: 'gixxer sf 250', maker: 'SUZUKI', weight: 184, class: 250 },
-  { name: 'R25', maker: 'YAMAHA', weight: 184, class: 250 },
-]
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
-  declarations: [ ],
+  declarations: [  ],
   imports:      [ ],
   providers: [],
-  bootstrap: []
+  bootstrap: [MatPaginator, MatTableModule, MatPaginatorModule, MatTableDataSource],
 })
 
-export class BikeModule implements OnInit {
-  displayedColumns: string[] = ['name', 'maker', 'weight', 'class']
-  // dataSource = new MatTableDataSource<PeriodicElemnt>(ELEMENT_DATA);
-
-  // @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
-
-  ngOnInit() {
-    // this.dataSource.paginator = this.paginator;
-  }
+export class BikeModule {
 }
