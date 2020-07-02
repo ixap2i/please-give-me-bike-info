47da74380757c31119d1a944722729fee7b53a85
ixap2i <ayamashita124+124@gmail.com>
2020-07-02 23:10:54 +0900

    add scraping function

--- server/server.ts
+++ server/server.ts
@@ -43,6 +43,39 @@ var options = {
   },
   json: true
 };
+
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+  await page.setViewport({ width: 1280, height: 1800 })
+  await page.goto('http://motorcycle.goobike.com/motorcycle/bike/suzuki.html')
+  var counter = {
+    index: null,
+    getCount: function() {
+      return this.index++;
+    }
+  }
+  const _50ccs = await page.$$eval('#main > form:nth-child(2) > div > dl:nth-child(2) > dd > ul > li', anchors => { return anchors.map(a => { return [a.textContent.trim(), a.firstChild.value] }) })
+  const _125ccs = await page.$$eval('#main > form:nth-child(2) > div > dl:nth-child(3) > dd > ul > li', anchors => { return anchors.map(a => { return [a.textContent.trim(), a.firstChild.value] }) })
+  const _250ccs = await page.$$eval('#main > form:nth-child(2) > div > dl:nth-child(4) > dd > ul > li', anchors => { return anchors.map(a => { return [a.textContent.trim(), a.firstChild.value] }) })
+  const _400ccs = await page.$$eval('#main > form:nth-child(2) > div > dl:nth-child(5) > dd > ul > li', anchors => { return anchors.map(a => { return [a.textContent.trim(), a.firstChild.value] }) })
+  console.log(_50ccs)
+  console.log(_125ccs)
+  console.log(_250ccs)
+  console.log(_400ccs)
+
+  await page.goto('http://motorcycle.goobike.com/web/motorcycle/summary.php?maker=&type=&exhaust1=&exhaust2=&model='+_250ccs[0][1]+'&baitai_name=&kind=&price_low=&price_high=&new_flg=&year_exhaust_flg=&nenshiki_start=&nenshiki_end=')
+  const specs = await page.$$eval('#result > table > tbody > tr > td.modelName', anchors => { return anchors.map(a => { return [a.textContent.trim(), a.children[0].href] }) })
+  console.log(specs)
+
+  await page.goto(specs[0][1])
+  const page_spec = await page.$$eval('#subCarInfoList', anchors => { return anchors.map(a => { return a.textContent.trim() }) })
+  console.log(page_spec)
+  await browser.close()
+})()
+
 app.get('/goo_bikes', (req, res) => {
   request.get(options,
   function(err, resp, requ) {
