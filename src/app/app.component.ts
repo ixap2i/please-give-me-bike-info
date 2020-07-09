7030486b907e176523f38f22e87c450e6fd74c87
ixap2i <ayamashita124+124@gmail.com>
2020-07-09 23:38:29 +0900

    add header view animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,17 +2,43 @@ import { Component, OnInit } from '@angular/core';
 
 import { Observable } from 'rxjs'
 import { map, startWith } from 'rxjs/operators'
-import {OverlayContainer} from '@angular/cdk/overlay';
+import { OverlayContainer } from '@angular/cdk/overlay';
 import { FormControl, FormGroup, Form, FormBuilder, Validators } from '@angular/forms';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 // ますとらしい
 import { HammerModule } from '@angular/platform-browser'
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.pug',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('hover-cursor', [
+      state('mouse-enter', style({
+        opacity: 1,
+        backgroundColor: '#000',
+        color: '#fff'
+      })),
+      state('mouse-out', style({
+        opacity: 1,
+        backgroundColor: 'rgba(196, 196, 196, 0.28)'
+      })),
+      transition('mouse-enter => mouse-out',[
+        animate('1s')
+      ]),
+      transition('mouse-out => mouse-enter',[
+        animate('1s')
+      ])
+    ])
+  ]
 })
 
 export class AppComponent implements OnInit {
