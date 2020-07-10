bbd9d0673bdc33f03686ee727c770e44fc285723
ixap2i <ayamashita124+124@gmail.com>
2020-07-10 15:51:13 +0900

    add bike component

---
+++ src/app/bike.menu.component.ts
@@ -0,0 +1,11 @@
+import { OnInit, Component } from '@angular/core';
+
+@Component({
+  selector: 'bike-menu',
+  styleUrls: ['./bike.menu.component.scss'],
+  templateUrl: './bike.menu.component.pug'
+})
+export class BikeMenuComponent {
+  constructor() {}
+}
+
